e99c126f714de3f64919b64ae1f5821529f4378a
CarlyJiaxinHe <68092672+jhe20@users.noreply.github.com>
2021-04-13 18:50:47 -0400

    Update data_cleansing.py

--- data_cleansing.py
+++ data_cleansing.py
@@ -1,12 +1,1 @@
-import pandas as pd
-import numpy as np
-import datetime
 
-games = pd.read_csv('games_flat_xml_2012-2018.csv')
-tv = pd.read_csv('TV_Ratings_onesheet.csv')
-
-games['Date'] = games['gameid&date'].str.slice(0, 10)
-games['gamesid'] = games['gameid&date'].str.slice(10,)
-
-tv = tv.rename(columns = {"VisTeamID":"visid",'HomeTeamID':"homeid"})
-pd.merge(left=tv,right=games,how='outer',on=['Date','visid','homeid'])
