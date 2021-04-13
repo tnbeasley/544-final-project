dcc469b840920c8a7d40fecbed2dae1e47cbeca1
tnbeasley <clp292@vols.utk.edu>
2021-04-13 18:35:14 -0400

    Added framework for app

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ import dash_bootstrap_components as dbc
 
 import pandas as pd
 import numpy as np
+
+app = dash.Dash(name = __name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = dbc.Container(
+
+)
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug = True)
