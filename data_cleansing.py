e3d74fd640ffbb6c25bac220266b91ae406c5349
tnbeasley <clp292@vols.utk.edu>
2021-04-14 11:05:32 -0400

    Drop duplicate columns. First draft of join. Write clean data to new data set.

--- data_cleansing.py
+++ data_cleansing.py
@@ -1,18 +1,20 @@
 # import relevant packages
 
 import pandas as pd
+import numpy as np
 
 
 # read in data
 
 ratings = pd.read_csv("TV_Ratings_onesheet.csv")
 ratings.columns = ratings.columns.str.replace(' ', '').str.lower()
+ratings_key_cols = ['hometeamid', 'date']
 
 games = pd.read_csv("games_flat_xml_2012-2018.csv")
 games.columns = games.columns.str.replace(' ', '').str.lower()
+game_key_cols = ['homeid', 'date']
 
 # drop unecessary columns
-
 badcols = []
 for col in games.columns:
     if 'official' in col:
@@ -27,8 +29,29 @@ for col in games.columns:
         badcols.append(col)
     elif 'matchupteam' in col:
         badcols.append(col)
+        
 
-games = games.drop(badcols, axis=1)
+dupcols = games.columns[[i in ratings.columns for i in games.columns]]
+dupcols = dupcols[[i not in game_key_cols for i in dupcols]].values.tolist()
+
+dropcols = badcols + dupcols
+
+games = games.drop(dropcols, axis=1)
 
 # join on home team id and date
-ratings
+df = pd.merge(
+    left = ratings,
+    right = games,
+    left_on = ratings_key_cols,
+    right_on = game_key_cols,
+    how = 'outer',
+    suffixes = ['_r','_g']
+)
+
+df = df.rename({'date':'DATE'}, axis='columns')
+
+df['HOMEID'] = df[ratings_key_cols[0]].combine_first(df[game_key_cols[0]])
+
+df.drop([ratings_key_cols[0]] + [game_key_cols[0]], axis = 1)
+df = df.set_index(['DATE', 'HOMEID'])
+df.to_csv('clean_data.csv')
