723e8aa0756a1437c8819f193f7fb1fd3ced2c07
Reilly Williams <rwill148@vols.utk.edu>
2021-04-14 08:40:03 -0400

    Update data_cleansing.py

--- data_cleansing.py
+++ data_cleansing.py
@@ -1,1 +1,29 @@
+# import relevant packages
 
+import pandas as pd
+
+
+# read in data
+
+ratings = pd.read_csv("TV_Ratings_onesheet.csv")
+games = pd.read_csv("games_flat_xml_2012-2018.csv")
+
+
+# drop unecessary columns
+
+badcols = []
+for col in games.columns:
+    if 'official' in col:
+        badcols.append(col)
+    elif 'rush' in col:
+        badcols.append(col)
+    elif 'pass' in col:
+        badcols.append(col)
+    elif 'penalties' in col:
+        badcols.append(col)
+    elif 'fumble' in col:
+        badcols.append(col)
+
+games = games.drop(badcols, axis=1)
+
+# join on ?
