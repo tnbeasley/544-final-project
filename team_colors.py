066e4adb9db6dcb9557a4fe74b9de7aff1fffbad
tnbeasley <clp292@vols.utk.edu>
2021-04-26 20:02:43 -0400

    Added team_colors file with team_colors function to import the team colors dictionary

---
+++ team_colors.py
@@ -0,0 +1,19 @@
+def team_colors():
+    teamColorsDict = {
+        'UA':'#A60C31', 
+        'AR':'#9D2235', 
+        'AU':'#0C2340',
+        'UF':'#003087', 
+        'UGA':'#BA0C2F', 
+        'UK':'#0033A0',
+        'LSU':'#461D7C',
+        'OM':'#CE1126', 
+        'MS':'#660000',
+        'MIZZU':'#F1B82D', 
+        'SCAR':'#73000A',
+        'TAMU':'#500000',
+        'TENN':'#f77f00',
+        'VANDY':'#A8996E'
+    }
+    
+    return teamColorsDict
